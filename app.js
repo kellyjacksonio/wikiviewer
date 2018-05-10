51bbe335b4c6c918196ff3a936abe3e4cf403a09
Kelly Jackson <hi@kellyjackson.io>
2018-05-10 14:22:06 -0500

    Add splice methods/clean up code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var request = require("request");
+const   express     = require("express"),
+        app         = express(),
+        bodyParser  = require("body-parser"),
+        request     = require("request");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/styles", express.static(__dirname + "/styles"));
 
 app.set("view engine", "ejs");
 
+//
 var articleTitle = [];
 var description = [];
 var articleLink = [];
@@ -24,19 +25,20 @@ app.post("/search", function(req, res) {
     var search = req.body.search;
     var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=" 
     + search + "&limit=10&namespace=0&format=json";
-    request(apiURL, function(error, response, body) {
-        if(!error && response.statusCode == 200 && search !== "") {
-            displayResults = true;
-            articleTitle = [];
-            description = [];
-            articleLink = [];
-            errorMessage = "";
+    request(apiURL, function(err, reqRes, body) {
+        if(!err && reqRes.statusCode == 200 && search !== "") {
             var parsedData = JSON.parse(body);
+            displayResults = true;
+            // clears arrays for new searches
+            articleTitle.splice(0, articleTitle.length);
+            description.splice(0, description.length);
+            articleLink.splice(0, articleLink.length);
+            errorMessage = "";
 
             parsedData[1].forEach(function(data) {
                 articleTitle.push(data);
             });
-            console.log(typeof articleTitle + "article titles");
+
             parsedData[2].forEach(function(data) {
                 description.push(data);
             });
@@ -51,8 +53,8 @@ app.post("/search", function(req, res) {
     });
 });
 
-app.get("*", function(req,res) {
-    res.send("Sorry, page not found. Stay on the paved road please."); 
+app.get("*", function(req, res) {
+    res.redirect("/"); 
  });
 
 app.listen(3000, function() {
